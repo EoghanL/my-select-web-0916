507d5d3091b9885d995969150f1ba9d6ce8ddb8c
Eoghan Leddy <eoghan.leddy@flatironschool.com>
2016-09-23 21:39:04 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+    if block_given?
+      counter = 0
+      ret_arr = []
+      while collection.length > counter
+        ret_arr << collection[counter] if yield( collection[counter] )
+        counter += 1
+      end
+      ret_arr
+    else
+      puts "I need, I said I need a goshdarned block!"
+    end
 end
